7fed28d69e291e7eb30f520cb0f7272bb4224b48
Tetsuya Minase <tetsuya.minasen@gmail.com>
2018-12-05 22:09:00 +0900

    Revert "serverソース最新化"

--- app/app.js
+++ app/app.js
@@ -1,71 +1,37 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-// ログ
-const winston = require('winston');
-require('winston-daily-rotate-file');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// router
-const apiRouter = require('./routes/api');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// const dbRouter = require('./routes/connectDb');
+// const fileRouter = require('./routes/getFile');
 
-const app = express();
+var app = express();
 
-// ログ設定
-const logFormat = winston.format.printf(info => {
-  return `${info.timestamp} ${info.level}: ${info.message}`;
-});
-const appLogRotate = new (winston.transports.DailyRotateFile)({
-  filename: 'log/server-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-  level: 'info'
-});
-const errorLogRotate = new (winston.transports.DailyRotateFile)({
-  filename: 'log/error-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-  level: 'error'
-});
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    logFormat
-  ),
-  transports: [
-    appLogRotate,
-    errorLogRotate
-  ]
-});
-
-// 最低限落ちないようにしておく
-process.on('uncaughtException', function(err) {
-  logger.error(err)
-});
-
-
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app')));
-app.use(helmet());
-app.disable('x-powered-by');
 
-// ルーティング
-app.use('/api', apiRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/db', dbRouter);
+// app.use('/file', fileRouter);
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  logger.error(`404 not found [url:${req.url}]`);
   next(createError(404));
 });
 
@@ -75,12 +41,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // log error
-  logger.error(`500 internal server error [err:${err}]`);
-
   // render the error page
   res.status(err.status || 500);
-  res.send(err);
+  res.render('error');
 });
 
 module.exports = app;
